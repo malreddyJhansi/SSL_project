b6d5ef6dc3e705eca2a7cae77110406d7c1e786b
malreddyJhansi <malreddyjhanu2001@gmail.com>
2025-10-27 18:14:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import os
 import sys
-from utils.logger_utils import logger
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# Fix for Databricks (no _file_ variable)
+BASE_DIR = os.getcwd()
 if BASE_DIR not in sys.path:
     sys.path.append(BASE_DIR)
 
-from run_ssl_monitor import main as run_ssl_job
+from utils.logger_utils import logger
+from utils.spark_utility import *
+from utils.pdf_report import *
 
-if __name__ == "__main__":
-    try:
-        logger.info("🟢 Launching SSL Monitoring Job...")
-        run_ssl_job()
-        logger.info("✅ SSL Monitoring Job finished successfully!")
-    except Exception as e:
-        logger.error(f"❌ SSL Monitoring Job failed: {str(e)}", exc_info=True)
+logger.info("SSL Certificate Monitoring Job Started")
